eb1721c5b61efb832c2bc7650ef7bfb7a978e0b6
R3N3GATE <sc195407@gmail.com>
2026-09-06 14:47:01 +0100

    Atualizar o database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,1 +1,15 @@
+const { Pool } = require("pg");
+require("dotenv").config();
 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+pool.on("error", (error) => {
+    console.error("Erro inesperado no PostgreSQL:", error);
+});
+
+module.exports = pool;
